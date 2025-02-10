88b6af06668287d3047d2e02ce53a935c3e41a9b
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-02-10 11:18:12 +0500

    Correct output achieve
    previously wrong iteration style was being use iteration style fix desire output achieve

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,9 @@
+
 array = [4,3,78,2,0,2]
 
 def bubble_sort(array)
-  for i in array do
-    for j in array do
+  for i in 0...(array.length - 1) do
+    for j in 0...(array.length - 1 - i) do
       if array[j] > array [j+1]
         temporary_variable = array[j+1]
         array [j+1] = array[j]
@@ -10,6 +11,8 @@ def bubble_sort(array)
       end
     end
   end
+  p array
 end
 
 bubble_sort(array)
+
