bfdf53ed5fd710a346585bd08fb6f678a371c9a5
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-02-10 11:21:38 +0500

    Comments add
    comments add to explain how iteration is working to make code more readable

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,9 @@
 array = [4,3,78,2,0,2]
 
 def bubble_sort(array)
+  # iterate form the index 0 to size of array - 1
   for i in 0...(array.length - 1) do
+  # iterate form the index 0 to size of array - 1 - i because after each iteration one element will be sorted
     for j in 0...(array.length - 1 - i) do
       if array[j] > array [j+1]
         temporary_variable = array[j+1]
